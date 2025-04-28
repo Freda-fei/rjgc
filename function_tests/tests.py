1e6ae57c9f56a056fe9fe7963781b8f4f970ae23
fifi <1584220053@qq.com>
2025-04-28 10:11:52 +0800

    message 14: add a new user

--- function_tests/tests.py
+++ function_tests/tests.py
@@ -83,9 +83,53 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Give a gift to Lisi')
         # 想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
         # 访问URL，发现他的代办事项列表还在
         # 满意的离开了
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #张三新键一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy flowers')
+
+        #他注意到清单有个唯一的url
+        zhangsan_list_url = self.browser.current_url
+        self.assertRegex(zhangsan_list_url,"/lists/.+")#(1)
+
+        #现在有一个新用户王五访问网站
+        #我们使用一个新的浏览器会话
+        #确保张三的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        #王五访问首页
+        #页面中看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertNotIn('Give a gift to Lisi',page_text)
+
+        #王五输入一个新的待办事项，创建一个清单
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy milk')
+
+        #王五获得了他唯一的Url
+        wangwu_list_url = self.browser.current_url
+        self.assertRegex(wangwu_list_url,'lists/.+')
+        self.assertNotEqual(wangwu_list_url,zhangsan_list_url)
+        
+        #这个页面还是没有张三的清单
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertIn('Buy milk',page_text)
+
+        #两个人都很满意，然后碎觉去
+
 # if __name__ == '__main__':
 #     unittest.main()
 
